b737c9c244f899e35bb6edec570201713aacddf2
idankonimbo <idan@konimbo.co.il>
2025-09-01 11:53:01 +0300

    initiall

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,10 @@ import { CollectionGroup } from './collections/CollectionGroup.js'
  * items from the selected collection.
  * The collection group will have a relationship field to select items from the selected collection.
  *
-*/
+ */
 export const collectionGroupPlugin = (config: Config): Config => {
   const existingCollections: string[] =
-    config.collections?.map((collection) => collection.slug) || []//gets all collections from payload.config [posts,media,...]
+    config.collections?.map((collection) => collection.slug) || [] //gets all collections from payload.config [posts,media,...]
 
   // Create options for the select field
   const collectionOptions: { label: string; value: string }[] = existingCollections.map((slug) => ({
